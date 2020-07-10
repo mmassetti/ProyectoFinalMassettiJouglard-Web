c672d423f0de996eea23a1764d2f7b1753924817
Matias Massetti <matiasmassetti@gmail.com>
2020-07-10 18:41:55 -0300

    add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,63 +7,31 @@ import withAuthUser from "../utils/pageWrappers/withAuthUser";
 import withAuthUserInfo from "../utils/pageWrappers/withAuthUserInfo";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Head from "next/head";
+import Navbar from "../components/navbar";
 
 const Index = (props: any) => {
-  const { AuthUserInfo } = props;
-  const authUser = get(AuthUserInfo, "AuthUser");
-
   return (
-    <>
-      <Header />
-      {!authUser ? (
-        <>
-          <div>not signed in.</div>
-          <div>
-            <Link href={"/login"}>
-              <a>[ log in ]</a>
-            </Link>
-          </div>
-          <p>
-            <Link href={"/signup"}>
-              <a>[ create account ]</a>
-            </Link>
-          </p>
-        </>
-      ) : (
-        <>
-          <pre className="text-xs">{JSON.stringify(authUser, null, 2)}</pre>
-          <p>
-            <Link href={"/account"}>
-              <a>[ account ]</a>
-            </Link>
-          </p>
-          <p>
-            <Link href={"/spaces"}>
-              <a>[ spaces ]</a>
-            </Link>
-          </p>
-        </>
-      )}
-      <>
-        <Footer />
-      </>
-    </>
+    <div className="divide-y divide-gray-200">
+      <Head>
+        <meta
+          name="twitter:description"
+          content="News content from the Tailwind CSS team."
+        />
+
+        <meta property="og:url" content="https://blog.tailwindcss.com" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Blog – Tailwind CSS" />
+        <meta
+          property="og:description"
+          content="News content from the Tailwind CSS team."
+        />
+
+        <title>Blog – Tailwind CSS</title>
+      </Head>
+      <Navbar />
+    </div>
   );
 };
 
-Index.propTypes = {
-  AuthUserInfo: PropTypes.shape({
-    AuthUser: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      emailVerified: PropTypes.bool.isRequired
-    }),
-    token: PropTypes.string
-  })
-};
-
-Index.defaultProps = {
-  AuthUserInfo: null
-};
-
-export default withAuthUser(withAuthUserInfo(Index));
+export default Index;
