44b97a98523edfb106b6574952d593f0bbdef9df
Matias Massetti <matiasmassetti@gmail.com>
2020-08-21 22:24:23 -0300

    menu de ayuda

--- components/FixedPlugin/FixedPlugin.js
+++ components/FixedPlugin/FixedPlugin.js
@@ -26,124 +26,7 @@ export default function FixedPlugin(props) {
           <i className="fa fa-cog fa-2x" />
         </div>
         <ul className="dropdown-menu">
-          <li className="header-title">SIDEBAR FILTERS</li>
-          <li className="adjustments-line">
-            <a className="switch-trigger">
-              <div>
-                <span
-                  className={
-                    props.bgColor === "white"
-                      ? "badge filter badge-white active"
-                      : "badge filter badge-white"
-                  }
-                  data-color="white"
-                  onClick={() => {
-                    props.handleColorClick("white");
-                  }}
-                />
-                <span
-                  className={
-                    props.bgColor === "purple"
-                      ? "badge filter badge-purple active"
-                      : "badge filter badge-purple"
-                  }
-                  data-color="purple"
-                  onClick={() => {
-                    props.handleColorClick("purple");
-                  }}
-                />
-                <span
-                  className={
-                    props.bgColor === "blue"
-                      ? "badge filter badge-blue active"
-                      : "badge filter badge-blue"
-                  }
-                  data-color="blue"
-                  onClick={() => {
-                    props.handleColorClick("blue");
-                  }}
-                />
-                <span
-                  className={
-                    props.bgColor === "green"
-                      ? "badge filter badge-green active"
-                      : "badge filter badge-green"
-                  }
-                  data-color="green"
-                  onClick={() => {
-                    props.handleColorClick("green");
-                  }}
-                />
-                <span
-                  className={
-                    props.bgColor === "red"
-                      ? "badge filter badge-red active"
-                      : "badge filter badge-red"
-                  }
-                  data-color="red"
-                  onClick={() => {
-                    props.handleColorClick("red");
-                  }}
-                />
-                <span
-                  className={
-                    props.bgColor === "orange"
-                      ? "badge filter badge-orange active"
-                      : "badge filter badge-orange"
-                  }
-                  data-color="orange"
-                  onClick={() => {
-                    props.handleColorClick("orange");
-                  }}
-                />
-              </div>
-            </a>
-          </li>
-          <li className="header-title">Images</li>
-          <li className={bgImage === imagine1 ? "active" : ""}>
-            <a
-              className="img-holder switch-trigger"
-              onClick={() => {
-                setBgImage(imagine1);
-                props.handleImageClick(imagine1);
-              }}
-            >
-              <img src={imagine1} alt="..." />
-            </a>
-          </li>
-          <li className={bgImage === imagine2 ? "active" : ""}>
-            <a
-              className="img-holder switch-trigger"
-              onClick={() => {
-                setBgImage(imagine2);
-                props.handleImageClick(imagine2);
-              }}
-            >
-              <img src={imagine2} alt="..." />
-            </a>
-          </li>
-          <li className={bgImage === imagine3 ? "active" : ""}>
-            <a
-              className="img-holder switch-trigger"
-              onClick={() => {
-                setBgImage(imagine3);
-                props.handleImageClick(imagine3);
-              }}
-            >
-              <img src={imagine3} alt="..." />
-            </a>
-          </li>
-          <li className={bgImage === imagine4 ? "active" : ""}>
-            <a
-              className="img-holder switch-trigger"
-              onClick={() => {
-                setBgImage(imagine4);
-                props.handleImageClick(imagine4);
-              }}
-            >
-              <img src={imagine4} alt="..." />
-            </a>
-          </li>
+          <li className="header-title">Ayuda</li>
 
           <li className="button-container">
             <div className="button-container">
@@ -153,7 +36,7 @@ export default function FixedPlugin(props) {
                 target="_blank"
                 fullWidth
               >
-                Download free!
+                Descargar app Android
               </Button>
             </div>
           </li>
@@ -165,7 +48,7 @@ export default function FixedPlugin(props) {
                 target="_blank"
                 fullWidth
               >
-                Get PRO version
+                Dar feedback
               </Button>
             </div>
           </li>
@@ -176,9 +59,10 @@ export default function FixedPlugin(props) {
               href="https://www.creative-tim.com/learning-lab/nextjs/overview/material-dashboard?ref=njsmd-fixed-plugin"
               target="_blank"
             >
-              Documentation
+              Ver Demo Youtube
             </Button>
           </li>
+
           <li className="adjustments-line" />
         </ul>
       </div>
--- components/PageChange/PageChange.js
+++ components/PageChange/PageChange.js
@@ -39,9 +39,7 @@ export default function PageChange(props) {
         <div className={classes.iconWrapper}>
           <CircularProgress className={classes.progress} />
         </div>
-        <h4 className={classes.title}>
-          Loading page contents for: {props.path}
-        </h4>
+        <h4 className={classes.title}>Cargando contenido para: {props.path}</h4>
       </div>
     </div>
   );
