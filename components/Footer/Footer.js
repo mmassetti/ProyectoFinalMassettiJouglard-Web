fb21c6a837e2ec5a0e50dcf1cd679d49c2b76743
Matias Massetti <matiasmassetti@gmail.com>
2020-08-21 22:38:26 -0300

    working on footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,7 @@
-/*eslint-disable*/
 import React from "react";
-import PropTypes from "prop-types";
-// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
-// core components
 import styles from "assets/jss/nextjs-material-dashboard/components/footerStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -19,22 +15,12 @@ export default function Footer(props) {
           <List className={classes.list}>
             <ListItem className={classes.inlineBlock}>
               <a href="#home" className={classes.block}>
-                Home
+                Panel principal
               </a>
             </ListItem>
             <ListItem className={classes.inlineBlock}>
               <a href="#company" className={classes.block}>
-                Company
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a href="#portfolio" className={classes.block}>
-                Portfolio
-              </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
-              <a href="#blog" className={classes.block}>
-                Blog
+                Otro menú
               </a>
             </ListItem>
           </List>
@@ -43,13 +29,12 @@ export default function Footer(props) {
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
             <a
-              href="https://www.creative-tim.com?ref=njsmd-footer"
+              href="https://collectednotes.com/cobertura-suelos-inta-uns"
               target="_blank"
               className={classes.a}
             >
-              Creative Tim
+              INTA/UNS
             </a>
-            , made with love for a better web
           </span>
         </p>
       </div>
