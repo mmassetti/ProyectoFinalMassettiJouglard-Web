4beeffcc80a2905803ab53108427cf713ff197de
Matias Massetti <matiasmassetti@gmail.com>
2020-08-24 00:07:40 -0300

    added TODO to file

--- pages/session/[session] ASI TIENE QUE IR PERO NO ME ANDA.js
+++ pages/session/[session] ASI TIENE QUE IR PERO NO ME ANDA.js
@@ -1,3 +1,6 @@
+//TODO: Los corchetes es para que la ruta sea dinamica, por ej /session/1 mapearia acá /session/2 tambien
+//TODO  Sin los corchetes (la forma actual), la ruta queda /session/session que obviamente NO queremos
+
 // import { useRouter } from "next/router";
 
 // export default function SessionDetail() {
