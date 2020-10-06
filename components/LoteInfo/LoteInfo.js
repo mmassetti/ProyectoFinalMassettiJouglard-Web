5fb0ae6caf5db74139c1315138adda243f90aa8c
Matias Massetti <matiasmassetti@gmail.com>
2020-10-06 18:16:32 -0300

    add react-grid-gallery

--- components/LoteInfo/LoteInfo.js
+++ components/LoteInfo/LoteInfo.js
@@ -4,15 +4,13 @@ import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-import ImagesCarousel from "components/LoteTabs/ImagesCarousel";
 import AccessTime from "@material-ui/icons/AccessTime";
 import CustomTabs from "components/CustomTabs/CustomTabs.js";
 import BugReport from "@material-ui/icons/BugReport";
 import Code from "@material-ui/icons/Code";
 import Cloud from "@material-ui/icons/Cloud";
-import Button from "components/CustomButtons/Button.js";
 import { makeStyles } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Gallery from "react-grid-gallery";
 
 const styles = {
   cardCategoryWhite: {
@@ -33,6 +31,33 @@ const styles = {
   },
 };
 
+const IMAGES = [
+  {
+    src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
+    thumbnail:
+      "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
+    thumbnailWidth: 243,
+    thumbnailHeight: 190,
+    caption: "286H (gratisography.com)",
+  },
+  {
+    src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
+    thumbnail:
+      "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
+    thumbnailWidth: 243,
+    thumbnailHeight: 190,
+    caption: "201H (gratisography.com)",
+  },
+  {
+    src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
+    thumbnail:
+      "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
+    thumbnailWidth: 244,
+    thumbnailHeight: 190,
+    caption: "Big Ben (Tom Eversley - isorepublic.com)",
+  },
+];
+
 const useStyles = makeStyles(styles);
 
 export default function LoteInfo(props) {
@@ -40,68 +65,78 @@ export default function LoteInfo(props) {
 
   const classes = useStyles();
 
-  const [handleOpen, setHandleOpen] = useState({ open: false });
-  const handleClick = () => {
-    setHandleOpen({ open: true });
+  const showImageInfo = (index) => {
+    console.log("show image info for index: ", index);
   };
-  const matches = useMediaQuery("(max-width:600px)");
 
   return (
-    <GridItem xs={12} sm={12} md={6}>
-      <Card chart>
-        <CardHeader color="primary">
-          <h4 className={classes.cardTitleWhite}>{data.description} </h4>
-        </CardHeader>
-        <GridItem xs={12} sm={12} md={12}>
-          <h5>
-            <strong>{data.images.length} imágenes</strong> y{" "}
-            <strong>{data.pasturas.length} pasturas</strong> asociadas
-          </h5>
-        </GridItem>
+    <>
+      <GridItem xs={12} sm={12} md={6}>
+        <Card chart>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>{data.description} </h4>
+          </CardHeader>
+          <GridItem xs={12} sm={12} md={12}>
+            <h5>
+              <strong>{data.images.length} imágenes</strong> y{" "}
+              <strong>{data.pasturas.length} pasturas</strong> asociadas
+            </h5>
+          </GridItem>
 
-        <CardBody>
-          <CustomTabs
-            title="Ver:"
-            headerColor="dark"
-            tabs={[
-              {
-                tabName: "Cubrimiento",
-                tabIcon: BugReport,
-                tabContent: <p>Cubrimiento</p>,
-              },
-              {
-                tabName: "Imágenes",
-                tabIcon: Code,
-                tabContent: (
-                  <GridItem xs={6} sm={6} md={6}>
-                    <CardFooter>
-                      <Button color="success" onClick={handleClick}>
-                        Ver imágenes
-                      </Button>
-                    </CardFooter>
-
-                    <ImagesCarousel
-                      isMobile={matches}
-                      handleOpen={handleOpen}
-                      setHandleOpen={setHandleOpen}
-                    />
-                  </GridItem>
-                ),
-              },
-              {
-                tabName: "Notas",
-                tabIcon: Cloud,
-                tabContent: <p>Notas</p>,
-              },
-            ]}
-          />
-        </CardBody>
-        <CardFooter chart>
-          <div className={classes.stats}>
-            <AccessTime /> Actualizado por última vez el 21/08/2020
-          </div>
-        </CardFooter>
-      </Card>
-    </GridItem>
+          <CardBody>
+            <CustomTabs
+              title="Ver:"
+              headerColor="dark"
+              tabs={[
+                {
+                  tabName: "Cubrimiento",
+                  tabIcon: BugReport,
+                  tabContent: <p>Cubrimiento</p>,
+                },
+                {
+                  tabName: "Imágenes",
+                  tabIcon: Code,
+                  tabContent: (
+                    <div
+                      style={{
+                        display: "block",
+                        minHeight: "1px",
+                        width: "100%",
+                        border: "1px solid #ddd",
+                        overflow: "auto",
+                      }}
+                    >
+                      <Gallery
+                        images={IMAGES}
+                        enableImageSelection={false}
+                        enableLightbox={false}
+                        onClickThumbnail={(index) => {
+                          showImageInfo(index);
+                        }}
+                      />
+                    </div>
+                  ),
+                },
+                {
+                  tabName: "Notas",
+                  tabIcon: Cloud,
+                  tabContent: <p>Notas</p>,
+                },
+              ]}
+            />
+          </CardBody>
+          <CardFooter chart>
+            <div className={classes.stats}>
+              <AccessTime /> Actualizado por última vez el 21/08/2020
+            </div>
+          </CardFooter>
+        </Card>
+      </GridItem>
+      <GridItem xs={12} sm={12} md={6}>
+        <h5>
+          <strong>Seleccióna una imágen</strong> para mostrar su información
+        </h5>
+      </GridItem>
+    </>
   );
 }
