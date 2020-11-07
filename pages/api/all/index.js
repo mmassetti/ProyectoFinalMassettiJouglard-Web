ef57c086d2255ea4c7ce982857b60ed4d0f17ec0
Matias Massetti <matiasmassetti@gmail.com>
2020-11-06 21:10:42 -0300

    create endpoint to retreive all sessions info

---
+++ pages/api/all/index.js
@@ -0,0 +1,59 @@
+import firebase from "../../../configuration/firebase";
+
+export default async (req, res) => {
+  let dataResult = [];
+
+  const sessions = await firebase
+    .collection("sessions")
+    .orderBy("date", "desc")
+    .get();
+
+  for (const session of sessions.docs) {
+    let sessionDetails = await session.data().ref.get();
+
+    let data = [];
+
+    for (const lote of sessionDetails.data().lotes) {
+      let lotesDetails = await firebase
+        .collection("lotesDetails")
+        .doc(lote.ref.id)
+        .get();
+
+      let pasturasData = [];
+      if (lotesDetails.data()) {
+        for (const pastura of lotesDetails.data().pasturas) {
+          let pasturasDetails = await firebase
+            .collection("pasturasDetails")
+            .doc(pastura.ref.id)
+            .get();
+
+          pasturasData.push({
+            data: pasturasDetails.data(),
+            pasturaDetailId: pasturasDetails.ref.id,
+          });
+        }
+      }
+
+      data.push(
+        Object.assign({
+          loteDetailId: lotesDetails.ref.id,
+          loteData: lotesDetails.data(),
+          pasturasData: pasturasData,
+        })
+      );
+    }
+    dataResult.push({
+      sessionData: {
+        description: sessionDetails.data().description,
+        user: sessionDetails.data().user,
+        creationDate: sessionDetails.data().date,
+        notes: sessionDetails.data().notes,
+      },
+      lotesAndPasturasData: {
+        data,
+      },
+    });
+  }
+
+  return res.json(dataResult);
+};
